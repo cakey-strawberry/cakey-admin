5da056f33a4d927879fabe539a853ed069719fc8
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2024-06-30 18:58:51 +0900

    chore: working test on commit lint & lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
   return (
     <>
       <div>
-        {"test purpose"}
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
