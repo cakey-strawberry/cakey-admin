0afa064dcad9f39cfe0c024ea2087cb09c71b56a
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2024-07-16 16:02:28 +0900

    fix: use api to get api token in useAutoSignIn

--- src/common/hooks/useAutoSignIn.tsx
+++ src/common/hooks/useAutoSignIn.tsx
@@ -1,27 +1,15 @@
 import { useSetAtom } from "jotai";
 import { useEffect, useState } from "react";
 
-import { DEV_ADMIN_TOKEN, ADMIN_TOKEN } from "@common/constants/constants";
+import { api } from "@common/service/api/api";
 import { authAtom } from "@common/store/atoms/authAtom";
 
 export function useAutoSignIn() {
-  const tokenKey =
-    process.env.NODE_ENV === "development" ? DEV_ADMIN_TOKEN : ADMIN_TOKEN;
-
   const [loading, setLoading] = useState(true);
   const setAuthState = useSetAtom(authAtom);
 
   useEffect(() => {
-    const storedToken = localStorage.getItem(tokenKey);
-
-    /**
-     * @TODO
-     * auto sign in route로
-     * 토큰 검증
-     *
-     * 검증 성공 -> setAuthState(true)
-     * 검증 실패 -> 토큰 삭제 후 setAuthState(false)
-     */
+    const storedToken = api.getAdminToken();
 
     if (storedToken) {
       setLoading(false);
@@ -30,7 +18,7 @@ export function useAutoSignIn() {
       setLoading(false);
       setAuthState(false);
     }
-  }, [setAuthState, tokenKey]);
+  }, [setAuthState]);
 
   return [loading];
 }
